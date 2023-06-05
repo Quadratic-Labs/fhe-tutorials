3e14d77922de45e9f6dd81063a72f1074509e823
Haroune Mohammedi <haroune.mohammedi@quadratic-labs.com>
2023-06-05 17:19:28 +0200

    Refactor[knn]: better variable names

--- TravelRouting/knn.py
+++ TravelRouting/knn.py
@@ -11,28 +11,28 @@ def min_int_array(a) -> int:
     """Return the minimum value of an array of ints using the `min_int` function"""
     return reduce(min_int, a)
 
-def knn(array):
-    """Return the K min values from the array
+def knn(distances):
+    """Return the index of the K minimum values (nearest neighbors) from the distances array
+    
     The output is an array with zeros but ones at the indexes of the K min values
     Note that the function could return more that K element if there are equal values
     """
     # if k is a parameter, we get: TypeError: 'Tracer' object cannot be interpreted as an integer
-    k = 3
+    k = 2
     inf = 9
-    N, = array.shape
+    N, = distances.shape
     result = [0] * N
 
     for _ in range(k):
         # Get the minimum value of the array
-        m = min_int_array(array)
+        minimum = min_int_array(distances)
         # Get a selector for the value
-        first = array == m
+        minimum_selector = distances == minimum
         # Add the selected index to the result array
-        result = result | first
+        result = result | minimum_selector
         # Get a selector for the remaining element
-        rem = (np.logical_not(first.astype(bool))).astype(int)
+        remaining_selector = (np.logical_not(minimum_selector.astype(bool))).astype(int)
         # set the selected value to inf
-        array = array * rem + first*inf
+        distances = distances * remaining_selector + minimum_selector*inf
     
     return result
-
